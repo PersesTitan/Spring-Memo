caf5549cfa7521f1fa9e20dcb6aed1d3b6a203a8
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-17 18:33:09 +0900

    Initial commit

---
+++ src/main/java/com/spring/Memo/MemoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.Memo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/Memo/MemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.Memo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
