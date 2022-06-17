e8a1989131dad27ce9a4393ac61836131fce367c
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-17 23:25:22 +0900

    Search 추가

--- src/main/java/com/spring/Memo/controller/MemoController.java
+++ src/main/java/com/spring/Memo/controller/MemoController.java
@@ -1,8 +1,27 @@
 package com.spring.Memo.controller;
 
+import com.spring.Memo.domain.Memo;
+import com.spring.Memo.repository.MemoRepository;
+import com.spring.Memo.service.MemoService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class MemoController {
+
+    private final MemoRepository memoRepository;
+    private final MemoService memoService;
+
+//    public Memo createMemo(@RequestBody Memo memo) {
+//        memoRepository.save(memo);
+//    }
+
+    public List<Memo> findSearch(String search) {
+        return memoService.findSearch(search);
+    }
 
 }
