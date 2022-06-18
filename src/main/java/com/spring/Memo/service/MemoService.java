c790fa021ea6b8320b4910dc86c509c17abd5744
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-18 14:08:37 +0900

    업데이트 메소드 추가

--- src/main/java/com/spring/Memo/service/MemoService.java
+++ src/main/java/com/spring/Memo/service/MemoService.java
@@ -1,6 +1,7 @@
 package com.spring.Memo.service;
 
 import com.spring.Memo.domain.Memo;
+import com.spring.Memo.domain.MemoDTO;
 import com.spring.Memo.repository.MemoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,12 +22,23 @@ public class MemoService {
         return memo.getId();
     }
 
+    @Transactional
+    public void update(Long id, MemoDTO memoDTO) {
+        Memo memo = memoRepository.findOne(id);
+        memoRepository.update(memo, memoDTO);
+    }
+
     public Memo findOne(Long id) {
         return memoRepository.findOne(id);
     }
 
     public List<Memo> findAll() {
         return memoRepository.findAll();
+    }
+
+    public void remove(Long id) {
+        Memo memo = memoRepository.findOne(id);
+        memoRepository.remove(memo);
     }
 
     //검색 로직
