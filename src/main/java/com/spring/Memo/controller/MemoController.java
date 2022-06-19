7b11db9f81e4e37a7b01b20f162544720bba3c44
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-19 20:22:44 +0900

    수정 로직, 메모 생성

--- src/main/java/com/spring/Memo/controller/MemoController.java
+++ src/main/java/com/spring/Memo/controller/MemoController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -17,56 +18,70 @@ public class MemoController {
 
     private final MemoService memoService;
 
-    @RequestMapping("index")
-    public String mainPage(Model model) {
-        List<Memo> memos = memoService.findAll();
-        memos.add(Memo.createMemo("a", "A"));
-        memos.add(Memo.createMemo("b", "B"));
-        model.addAttribute("memos", memos);
-        return "index";
+    //메인 페이지
+    @RequestMapping({"/", "","index", "index.html"})
+    public String mainPage() {
+        return "redirect:/list";
     }
 
-    @RequestMapping("list")
+    @RequestMapping({"list", "memo_list", "memo_list.html"})
     public String memoList(Model model) {
         List<Memo> memos = memoService.findAll();
-        memos.add(Memo.createMemo("a", "A"));
-        memos.add(Memo.createMemo("b", "B"));
         model.addAttribute("memos", memos);
         return "memo_list";
     }
 
-    @RequestMapping("create")
-    public String create(Model model, MemoDTO memoDTO) {
-        model.getAttribute("");
-        Memo memo = Memo.createMemo(memoDTO.title(), memoDTO.content());
-
-        return "new_list";
+    //메모 생성
+    @GetMapping("add")
+    public String createForm() {
+        return "new_memo";
     }
 
-//    @GetMapping("save")
-//    public String save() {
-//        memoService.save(Memo.createMemo("a", "a"));
-//        memoService.save(Memo.createMemo("b", "b"));
-//        System.out.println(memoService.findAll());
-//        return "redirect:/";
-//    }
+    @PostMapping("add")
+    public String createMemo(Memo memo, RedirectAttributes redirectAttributes) {
+        Long id = memoService.save(memo);
+        redirectAttributes.addAttribute("id", id);
+        redirectAttributes.addAttribute("create", true);    //생성했을때 메세지
+        return "redirect:/memo/{id}";
+    }
 
+    //수정 로직
     @GetMapping("memo/{id}")
     public String memo(@PathVariable Long id, Model model) {
         Memo memo = memoService.findOne(id);
-        model.addAttribute("id", memo);
+        model.addAttribute("title", memo.getTitle());
+        model.addAttribute("content", memo.getContent());
         return "item/memo";
     }
 
+    //삭제 로직
     @DeleteMapping("memo/{id}")
     public Long remove(@PathVariable Long id) {
         memoService.remove(id);
         return id;
     }
 
-    @PutMapping("memo/{id}")
-    public Long update(@PathVariable Long id, @RequestBody MemoDTO memoDTO) {
+    //수정 로직
+    @GetMapping("edit/{id}")
+    public String editFrom(@PathVariable Long id, Model model) {
+        Memo memo = memoService.findOne(id);
+        model.addAttribute("memo", memo);
+        return "edit_memo";
+    }
+
+    @PostMapping("edit/{id}")
+    public String edit(@PathVariable Long id, @ModelAttribute MemoDTO memoDTO) {
         memoService.update(id, memoDTO.title(), memoDTO.content());
-        return id;
+        return "redirect:/memo/{id}";
+    }
+
+    //테스트용 생성 로직
+    @PostConstruct
+    public void init() {
+        Memo memo1 = Memo.createMemo("a", "A");
+        Memo memo2 = Memo.createMemo("b", "B");
+        
+        memoService.save(memo1);
+        memoService.save(memo2);
     }
 }
