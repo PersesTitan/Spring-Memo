a0ecf75b9d6a77305f55e2db496436095e2b6cf6
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-20 08:09:00 +0900

    주석 추가

--- src/main/java/com/spring/Memo/controller/MainController.java
+++ src/main/java/com/spring/Memo/controller/MainController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainController {
 
     //메인 페이지
+    //추후 기능 추가시 redirect 삭제 후 기능 추가
     @RequestMapping({"/", "","index", "index.html"})
     public String mainPage() {
         return "redirect:/memos";
