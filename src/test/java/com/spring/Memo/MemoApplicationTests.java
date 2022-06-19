bd4548583872796666f6c88804e05413afc53b69
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-19 20:21:48 +0900

    테스트 추가

--- src/test/java/com/spring/Memo/MemoApplicationTests.java
+++ src/test/java/com/spring/Memo/MemoApplicationTests.java
@@ -1,13 +1,28 @@
 package com.spring.Memo;
 
+import com.spring.Memo.domain.Memo;
+import com.spring.Memo.service.MemoService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MemoApplicationTests {
 
+	@Autowired
+	private MemoService memoService;
+
 	@Test
 	void contextLoads() {
+		//given
+		Memo memo = Memo.createMemo("A", "a");
+		Memo memo1 = Memo.createMemo("B", "b");
+
+		//when
+		memoService.save(memo);
+		memoService.save(memo1);
+
+		//then
 	}
 
 }
